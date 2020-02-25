c51b8e13cbf36635b91d6a451576d5812f35bd7c
ohya <ohya@localhost.localdomain>
2020-02-26 00:48:18 +0900

    ModuleInfoクラスの追加

--- praqta/command_factory.py
+++ praqta/command_factory.py
@@ -1,18 +1,33 @@
-import typing
 from commands.command_base import CommandBase
 import commands.sample.command
 import importlib
-import yaml
+import os
 import os.path
+import typing
+import yaml
+import time
+
+
+class ModuleInfo(object):
+
+    def __init__(self, name, module):
+        self.__module = module
+        file = module.__file__
+        (path, _) = os.path.splitext(file)
+        with open(path + '.yml', 'r') as f:
+            self.__spec = yaml.load(f)
+        self.__file = file
+        self.__timestamp = os.stat(self.__file).st_mtime
+
+    def is_updated(self):
+        return os.stat(self.__file).st_mtime > self.__timestamp
+
+    def get_module(self):
+        return self.__module
+
 
 # モジュールのキャッシュ
 __command_module_cache = {}
-__command_specs = {}
-
-
-def load_spec(key: str, path: str):
-    with open(path, 'r') as f:
-        __command_specs[key] = yaml.load(f)
 
 
 def create_command(command_name: str) -> CommandBase:
@@ -29,16 +44,20 @@ def create_command(command_name: str) -> CommandBase:
     commandInstance : CommandBase
     """
     key = f'commands.{command_name}.command'
-
+    need_load = True
     if key in __command_module_cache:
         # キャッシュからモジュールを取得
         module = __command_module_cache[key]
-        print("get module from cache.")
-    else:
-        # モジュールを動的にロード
-        module = importlib.import_module(key)
-        __command_module_cache[key] = module
-        (path, ext) = os.path.splitext(module.__file__)
-        path + ".py"
+        need_load = False
+        # モジュールのタイムスタンプが新しくなっていたら、モジュール再ロード
+        if module.is_updated():
+            need_load = True
 
-    return module.new_instance()
+    if need_load:
+        # モジュールを動的にロード
+        module = ModuleInfo(command_name,
+                            importlib.import_module(key))
+        # キャッシュに格納
+        __command_module_cache[key] = module
+
+    return module.get_module() .new_instance()

58cccf8075d3339ac2404c155c17f4e83354a048
ohya <ohya@localhost.localdomain>
2020-03-11 22:14:13 +0900

    スクリプトのコマンドパラメータのバリデーション機能を追加 各コマンドのパラメータ定義を見直し

---
+++ packages/services/praqta/arg_checker.py
@@ -0,0 +1,110 @@
+from praqta.interface import Row
+
+
+class ArgSpec(object):
+    def __init__(self, name: str, typeName: str, values: list, comment: str, require: bool, default):
+        self.name = name
+        self.type = typeName
+        self.values = values
+        self.comment = comment
+        self.require = require
+        self.default = default
+
+    def validate(self, value) -> bool:
+
+        if self.type == 'enum':
+            if not value in self.values:
+                hint = ','.join(self.values)
+                raise Exception(f'{self.name}の設定値は {hint}から選択してください。')
+
+        if self.type == 'list':
+            if type(value) != list:
+                raise Exception(f'{self.name}の設定値は list型を指定してください。')
+
+        if self.type in ['dict', 'object']:
+            if not type(value) in [dict, object]:
+                raise Exception(f'{self.name}の設定値は 辞書型を指定してください。')
+
+        return True
+
+
+class ArgSpecs(object):
+    def __init__(self, commandName):
+        self.commandName = commandName
+        self.specs = {}
+        self.requires = []
+
+    def append(self, spec: ArgSpec):
+        if spec.require == True:
+            self.requires.append(spec)
+        self.specs[spec.name] = spec
+
+    def validate(self, params):
+
+        r = Row(params)
+
+        # 必須プロパティチェック
+        for v in self.requires:
+            if r.get(v.name) == None:
+                raise Exception(
+                    f'{self.commandName}: {v.name}プロパティが設定されていません。')
+
+        # パラメータのキーを取得
+        for key in params.keys():
+            # パラメータ仕様にないパラメータ
+            if not key in self.specs:
+                raise Exception(
+                    f'{self.commandName}: {key}プロパティは、パラメータ仕様に定義されていません。')
+            # 値を取得
+            value = r.get(key)
+            # 値のチェックを行う
+            self.specs[key].validate(value)
+
+
+def validate_spec(commandName: str, specDict: dict, logger) -> dict:
+    spec = Row(specDict)
+    if spec.get('name') == None:
+        logger.error(f'{commandName}.yml: nameプロパティが定義されていません')
+
+    if spec.get('comment') == None:
+        logger.error(f'{commandName}.yml: commentプロパティが定義されていません')
+
+    if spec.get('parameters') == None:
+        logger.error(f'{commandName}.yml: parameterプロパティが定義されていません')
+
+    if spec.get('sample') == None:
+        logger.error(f'{commandName}.yml: sampleプロパティが定義されていません')
+
+    ret = ArgSpecs(commandName)
+    for name in spec.get('parameters').keys():
+        param = Row(spec.get('parameters')[name])
+
+        if param.get('type') == None:
+            logger.error(f'{commandName}.yml: {name}.typeが指定されていません')
+
+        if not param.get('type') in ['string', 'int', 'bool', 'enum', 'list', 'object', 'dict']:
+            logger.error(
+                f'{commandName}.yml: {name}.type は、string,int,bool,enum,list,dict,objectの何れかを指定してください')
+
+        if param.get('type') == 'enum':
+            if param.get('values') == None:
+                logger.error(
+                    f'{commandName}.yml: {name}.type がenumの時は、列挙値をvaluesに配列で記述してください')
+
+        if param.get('comment') == None:
+            logger.error(
+                f'{commandName}.yml: {name}.comment が定義されていません。パラメータの説明を記述してください')
+
+        if param.get('require') == None:
+            logger.error(f'{commandName}.yml: {name}.require が定義されていません。')
+
+        if param.get('default') == None:
+            logger.error(f'{commandName}.yml: {name}.default が定義されていません。')
+
+        ret.append(ArgSpec(name,
+                           param.get('type'),
+                           param.get('values'),
+                           param.get('comment'),
+                           param.get('require'),
+                           param.get('default')))
+    return ret
